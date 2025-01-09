f1bc6826684f80163ba8f8b12723eb596d03d1eb
zoykh1 <kerimgmc45@gmail.com>
2025-01-10 00:57:03 +0100

    fixing bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,13 +10,14 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 	int temp;
+	int swapped;
 
 	if (!array || size < 2)
-
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -24,8 +25,11 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+		if (!swapped)
+			break;
 	}
 }
